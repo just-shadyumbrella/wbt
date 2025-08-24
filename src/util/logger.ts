f56da07d7073fbbe7da8db6a747040ca748bb137
just-shadyumbrella <xshadyumbrella@proton.me>
2025-08-24 23:56:55 +0700

    Fresh start: preserve current state, reset history

---
+++ src/util/logger.ts
@@ -0,0 +1,62 @@
+import chalk from 'chalk'
+import { execSync } from 'node:child_process'
+
+chalk.level = 1
+
+export enum LoggerType {
+  LOG,
+  INFO,
+  WARN,
+  ERROR,
+}
+
+export function logger(
+  type: LoggerType,
+  module: { name: string; fn: string; context?: string; padding?: number },
+  ...data: any[]
+) {
+  const consol = {
+    [LoggerType.LOG]: {
+      log: 'log',
+      chalk: chalk.white,
+    },
+    [LoggerType.INFO]: {
+      log: 'info',
+      chalk: chalk.cyan,
+    },
+    [LoggerType.WARN]: {
+      log: 'warn',
+      chalk: chalk.yellow,
+    },
+    [LoggerType.ERROR]: {
+      log: 'error',
+      chalk: chalk.red,
+    },
+  }
+  if (type === LoggerType.WARN) playBeep('background')
+  if (type === LoggerType.ERROR) playBeep('foreground')
+  if (typeof data[0] === 'string') data[0] = consol[type].chalk(data[0])
+  return console[consol[type].log](
+    consol[type].chalk(leftAlignWithSpaces(consol[type].log.toUpperCase(), consol[LoggerType.ERROR].log.length)),
+    `[${chalk.magenta(new Date().toISOString())}]`,
+    leftAlignWithSpaces(
+      `[${chalk.green(`${module.name}:${module.fn}${module.context ? `?${module.context}` : ''}`)}]`,
+      module.padding
+    ),
+    ...data
+  )
+}
+
+export function leftAlignWithSpaces(text: string, maxlength = 42) {
+  const cleanText = text.trim()
+  const r = maxlength >= cleanText.length ? maxlength - cleanText.length : 0
+  return `${text}${' '.repeat(r)}`
+}
+
+function playBeep(type: 'foreground' | 'background') {
+  if (process.platform === 'win32' && type === 'background') {
+    execSync('rundll32 user32.dll,MessageBeep')
+    return
+  }
+  process.stdout.write('\x07') // Under VSCode won't work
+}
